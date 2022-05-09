e637e3fd76cbd50d55bbc1fbb32fe40bcdd60334
Kingerburg <96078584+Kingerburg@users.noreply.github.com>
2022-05-09 22:37:05 +0300

    feat: fix save localstorage

--- virtual-keyboard/scripts.js
+++ virtual-keyboard/scripts.js
@@ -19,11 +19,24 @@ let rusNameKeySup = ['', '!', '"', '№', ';', '', ':', '?', '*', '', '', '', ''
 let nameAttributes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
 let language = 'eng';
-let locallanguage;
+let locallanguage = localStorage.getItem('Language')
+
+
 let getLanguage = () => {
     locallanguage = localStorage.getItem('Language')
 }
+
 getLanguage();
+
+if (locallanguage == null || locallanguage == 'eng') {
+    language = 'eng'
+    localStorage.setItem('Language', language)
+    getLanguage();
+} else if (locallanguage == 'rus') {
+    language = 'rus'
+    localStorage.setItem('Language', language)
+    getLanguage();
+}
 
 let createKeyButton = () => {
 
